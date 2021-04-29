ad5747b4391ac3591484c734d870920bd34f1f59
pengyu <hpy@bczx365.com>
2021-04-29 18:32:37 +0800

    update

--- preprocess_package/cut_images.py
+++ preprocess_package/cut_images.py
@@ -7,11 +7,11 @@ pwd = os.getcwd()
 image_index = 1
 
 
-def cut_images_save(img, if_show_pre=False, img_name='', save_path='./results/crops/'):
+def cut_images_save(img, if_show_pre=False, if_show=False, img_name='', save_path=''):
     global image_index
     print('**********************开始处理图片*************************')
     print('img_name: ', img_name)
-    wrap = detect_image_counts(img, if_show_pre, img_name)
+    wrap = detect_image_counts(img, if_show_pre, if_show, img_name)
     print('wrap: ', wrap)
     if wrap == 2:
         print('-------------此图片有两张发票----------------')
@@ -31,4 +31,3 @@ if __name__ == '__main__':
 
     img = cv2.imread(file_path)
     cut_images_save(img, False, 'ss', crops_save_path)
-
