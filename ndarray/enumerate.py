b00bb10f52a831c90ede9bfc97441fb296faba5f
pengyu <hpy@bczx365.com>
2021-04-29 17:17:02 +0800

    update

---
+++ ndarray/enumerate.py
@@ -0,0 +1,4 @@
+line = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+line1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i, j, m in enumerate(line, line1):
+    print(i, j, m)
--- preprocess_package/cut_images.py
+++ preprocess_package/cut_images.py
@@ -7,7 +7,7 @@ pwd = os.getcwd()
 image_index = 1
 
 
-def cut_images_save(img, if_show_pre, img_name, save_path):
+def cut_images_save(img, if_show_pre=False, img_name='', save_path='./results/crops/'):
     global image_index
     print('**********************开始处理图片*************************')
     print('img_name: ', img_name)
@@ -26,9 +26,9 @@ def cut_images_save(img, if_show_pre, img_name, save_path):
 
 
 if __name__ == '__main__':
-    file_path = './pictures/Image_00009.jpg'
+    file_path = './pictures/2021_4_29.jpg'
     crops_save_path = './results/crops/'
 
     img = cv2.imread(file_path)
-    cut_images_save(img, crops_save_path)
+    cut_images_save(img, False, 'ss', crops_save_path)
 
