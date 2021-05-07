998d300028912d818087aff50c57d9d8286a6f4c
pengyu <hpy@bczx365.com>
2021-05-07 13:40:34 +0800

    update

--- ndarray/list_().py
+++ ndarray/list_().py
@@ -1,5 +1,10 @@
-a = []
-a.append((1, 2))
-a.append((1, 2))
-a.append((1, 2))
-print(a)
+movie_name = ['加勒比海盗', '骇客帝国', '第一滴血', '指环王', '霍比特人', '速度与激情']
+print('------删除之前--------')
+for temp in movie_name:
+
+    print(temp)
+    del movie_name[2]
+print('--------删除之后---------')
+for temp in movie_name:
+    print(temp)
+
--- str/re_the_after.py
+++ str/re_the_after.py
@@ -6,7 +6,8 @@ banks = ['开户行及账号：江苏银行徐州经济开发区支行6002018800
 banks1 = ['开户行及账号：yi60020188000023518']
 banks2 = ['开户行及账号：江苏银行徐州经济开发区支行60020188000023518']
 
-tmp = ['开户行及账号60020188000023518']
+tmp = ['开户行及账号：但是方式60020188000023518']
+
 
 
 def print_banks(banks):
@@ -19,13 +20,14 @@ def print_banks(banks):
 def print_bank1s(banks):
     for bank in banks:
         # bank_count_findall = re.findall('((?<=支行)\d{12,25})$', bank)
-        bank_count_match = re.search('^开户行及账号：.*\d{12,25}$', bank)
+        purchaser_bank_num = re.search('^开户行及账号：.*\d{12,25}$', bank)
+        # purchaser_bank_num = re.search('.*\d{12,25}$', bank)
         # bank_count_match = re.search('^开户行及帐号：\d{12,25}$', bank).group()
-        if bank_count_match is not None:
-            bank_count_match = bank_count_match.group()
-            print(bank_count_match)
+        if purchaser_bank_num is not None:
+            purchaser_bank_num = purchaser_bank_num.group().replace('开户行及账号：', '')
+            print(purchaser_bank_num)
 
 
 if __name__ == '__main__':
     # print_banks(banks)
-    print_bank1s(banks)
+    print_bank1s(tmp)
