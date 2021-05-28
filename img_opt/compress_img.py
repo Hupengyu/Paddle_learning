71b3495e854c93d2006391cdae11d61bda25c936
pengyu <hpy@bczx365.com>
2021-05-28 14:49:12 +0800

    compress_img通用接口

--- img_opt/compress_img.py
+++ img_opt/compress_img.py
@@ -2,18 +2,43 @@ from PIL import Image
 import cv2
 import os
 
-file = 'file/001_270.jpg'
-# img = Image.open(file)
-# w, h = img.size
-# w, h = round(w), round(h)  # 去掉浮点，防报错
-img = cv2.imread(file)
-img_size = os.path.getsize(file)/1024/1024
-img_height, img_width = img.shape[0:2]
 
-while img_size > 1:
-    # 压缩图片
-    img.thumbnail((int(img_width/2), int(img_height/2)), Image.ANTIALIAS)
-    img.save('results/1.jpg','jpg')  # 质量
-    img_size = os.path.getsize(file) / 1024 / 1024
+def compress_img(image, img_path, cover_ori_img=True, img_size_thresh_KB=1000):
+    os_size = os.path.getsize(img_path)  # 返回字节数
+    img_size = os_size / 1024
+    if img_size < img_size_thresh_KB:
+        return image
+    else:
+        if cover_ori_img:
+            image = image.copy()    # 将原图复制，使用copy的图片，并最后重新命名
+            img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            img_width, img_height = img.size
+            while img_size > img_size_thresh_KB:
+                img.thumbnail((int(img_width / 2), int(img_height / 2)), Image.ANTIALIAS)
+                ori_img_name, img_suffix = os.path.splitext(img_path)  # ('imgs/01_oppo', '.jpg')
+                copy_img_path = ori_img_name + '_copy' + img_suffix
+                img.save(copy_img_path)
+                img = Image.open(copy_img_path)
+                img_width, img_height = img.size
+                img_size = os.path.getsize(copy_img_path) / 1024  # 返回字节数
+            img_compressed = cv2.imread(copy_img_path)
+            return img_compressed
+        else:
+            img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            img_width, img_height = img.size
+            while img_size > img_size_thresh_KB:
+                img.thumbnail((int(img_width/2), int(img_height/2)), Image.ANTIALIAS)
+                img.save(img_path)
+                img = Image.open(img_path)
+                img_width, img_height = img.size
+                img_size = os.path.getsize(img_path) / 1024    # 返回字节数
+            img_compressed = cv2.imread(img_path)
+            return img_compressed
 
-cv2.imwrite('results/1.jpg', img)
+
+if __name__ == '__main__':
+    img_path = 'imgs/01_oppo.jpg'
+    img = cv2.imread(img_path)
+    img_compressed = compress_img(img, img_path)    # 默认覆盖，默认大小为1000KB
+    cv2.imshow('aa', img_compressed)
+    cv2.waitKey()
---
+++ str/img_name_change.py
@@ -0,0 +1,5 @@
+import os
+
+img_path = 'imgs/01_oppo.jpg'
+name1 = os.path.splitext(img_path)
+print(name1)
